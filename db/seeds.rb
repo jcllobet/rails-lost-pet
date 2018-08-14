c24cb19b878550936cd678d53aa91624a406d9d3
Jan Carbonell <jcllobet@gmail.com>
2018-08-14 17:31:01 +0200

    initial seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Pet.create(name: "Fluffy", species: "Dog", found_on: Time.zone.today - 5)
+Pet.create(name: "Stuart", species: "Cat", found_on: Time.zone.today - 4)
+Pet.create(name: "Minar", species: "Dog", found_on: Time.zone.today - 1)
